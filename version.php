ddc0a98afbd93b5b751d728e967871d9a0117a79
valery <valery.fremaux@gmail.com>
2017-11-18 13:43:33 +0100

    update readmes

--- version.php
+++ version.php
@@ -27,11 +27,11 @@
 defined('MOODLE_INTERNAL') || die;
 
 $plugin->version   = 2016030800;        // The current plugin version (Date: YYYYMMDDXX).
-$plugin->requires  = 2016051900;        // Requires this Moodle version.
+$plugin->requires  = 2016112900;        // Requires this Moodle version.
 $plugin->component = 'report_vmoodle'; // Full name of the plugin (used for diagnostics).
-$plugin->release = '3.1.0 (Build 2016030800)';
+$plugin->release = '3.2.0 (Build 2016030800)';
 $plugin->maturity = MATURITY_STABLE;
 
 // Non moodle attributes.
-$plugin->codeincrement = '3.1.0000';
+$plugin->codeincrement = '3.2.0000';
 
