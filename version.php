a2bd9091c1804f3be8377fa22d7f865e9b25ab38
valery <valery.fremaux@gmail.com>
2018-01-27 21:48:35 +0100

    first code load for M34

--- version.php
+++ version.php
@@ -27,15 +27,6 @@
 defined('MOODLE_INTERNAL') || die;
 
 $plugin->version   = 2016030800;        // The current plugin version (Date: YYYYMMDDXX).
-<<<<<<< HEAD
-$plugin->requires  = 2017050500;        // Requires this Moodle version.
-$plugin->component = 'report_vmoodle'; // Full name of the plugin (used for diagnostics).
-$plugin->release = '3.3.0 (Build 2016030800)';
-$plugin->maturity = MATURITY_STABLE;
-
-// Non moodle attributes.
-$plugin->codeincrement = '3.3.0000';
-=======
 $plugin->requires  = 2017110800;        // Requires this Moodle version.
 $plugin->component = 'report_vmoodle'; // Full name of the plugin (used for diagnostics).
 $plugin->release = '3.4.0 (Build 2016030800)';
@@ -43,5 +34,4 @@ $plugin->maturity = MATURITY_STABLE;
 
 // Non moodle attributes.
 $plugin->codeincrement = '3.4.0000';
->>>>>>> MOODLE_34_STABLE
 
