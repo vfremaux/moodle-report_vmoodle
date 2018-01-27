14835e2be4a502b69d453980a867b3b7bb68e72d
valery <valery.fremaux@gmail.com>
2018-01-27 21:43:43 +0100

    global resync

--- version.php
+++ version.php
@@ -23,15 +23,14 @@
  * @copyright   2010 onwards Valery Fremaux (http://www.mylearningfactory.com)
  * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-
 defined('MOODLE_INTERNAL') || die;
 
 $plugin->version   = 2016030800;        // The current plugin version (Date: YYYYMMDDXX).
-$plugin->requires  = 2012110900;        // Requires this Moodle version.
+$plugin->requires  = 2014110400;        // Requires this Moodle version.
 $plugin->component = 'report_vmoodle'; // Full name of the plugin (used for diagnostics).
-$plugin->release = '2.7.0 (Build 2016030800)';
+$plugin->release = '2.8.0 (Build 2016030800)';
 $plugin->maturity = MATURITY_STABLE;
 
 // Non moodle attributes.
-$plugin->codeincrement = '2.7.0000';
+$plugin->codeincrement = '2.8.0000';
 
