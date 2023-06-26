df918e3534292a203cd0aae6078b85c356600d56
Valery Fremaux <valery.fremaux@gmail.com>
2023-06-26 09:06:29 +0200

    Automated sync from SVN

--- version.php
+++ version.php
@@ -26,13 +26,14 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2016030800;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2023062605;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2020060900;        // Requires this Moodle version.
 $plugin->component = 'report_vmoodle'; // Full name of the plugin (used for diagnostics).
-$plugin->release = '3.9.0 (Build 2016030800)';
+$plugin->release = '4.1.0 (Build 2023062605)';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = array('local_vmoodle' => 2017090101);
+$plugin->supported = [401, 402];
 
 // Non moodle attributes.
-$plugin->codeincrement = '3.9.0001';
+$plugin->codeincrement = '4.1.0001';
 
